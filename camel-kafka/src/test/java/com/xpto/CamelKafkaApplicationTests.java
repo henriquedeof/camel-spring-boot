73147843a4e953a0fffdf04fc00bb9edf2a1f67b
henriquedeof <henriquedeof@gmail.com>
2023-01-04 23:02:28 +1100

    First commit

---
+++ camel-kafka/src/test/java/com/xpto/CamelKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
